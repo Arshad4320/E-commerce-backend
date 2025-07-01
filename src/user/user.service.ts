093a90f5a731a63906c3fa8b060dcbad5fbcf636
Arshad Ullah <mdarshadullah835@gmail.com>
2025-07-01 16:05:02 +0600

    user services done

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,31 +4,85 @@ import { User } from "./user.model";
 import bcrypt from "bcrypt";
 const jwtSecret = process.env.JWT_SECRET as string;
 const registerUserFromIntoDb = async (payload: IUser) => {
-  const isExits = await User.findOne({ email: payload.email });
-  if (isExits) {
-    return "user already exits";
+  try {
+    const isExits = await User.findOne({ email: payload.email });
+    if (isExits) {
+      return "user already exits";
+    }
+    payload.password = await bcrypt.hash(payload.password, 10);
+    const data = await User.create(payload);
+    return data;
+  } catch (err) {
+    console.log(err);
   }
-  payload.password = await bcrypt.hash(payload.password, 10);
-  const data = await User.create(payload);
-  return data;
 };
 const loginFromUserIntoDb = async (payload: {
   email: string;
   password: string;
 }) => {
-  const user = await User.findOne({ email: payload.email });
-  if (!user) {
-    return "user not found please create account or try again";
+  try {
+    const user = await User.findOne({ email: payload.email });
+    if (!user) {
+      return "user not found please create account or try again";
+    }
+    const match = await bcrypt.compare(user.password, payload.password);
+    if (match) {
+      return "wrong password,please try again";
+    }
+    const token = jwt.sign(
+      { email: user.email, password: user.password, userId: user._id },
+      jwtSecret,
+      { expiresIn: "30d" }
+    );
+    const result = await User.create({ user, token });
+    return result;
+  } catch (err) {
+    console.log(err);
   }
-  const match = await bcrypt.compare(user.password, payload.password);
-  if (match) {
-    return "wrong password,please try again";
+};
+const getUsersFormIntoDb = async () => {
+  try {
+    const result = await User.find();
+    const buyer = await User.find({ role: "buyer" });
+
+    const seller = await User.find({ role: "seller" });
+
+    const admin = await User.find({ role: "admin" });
+
+    return { result, buyer, seller, admin };
+  } catch (err) {
+    console.log(err);
   }
-  const token = jwt.sign(
-    { email: user.email, password: user.password, userId: user._id },
-    jwtSecret,
-    { expiresIn: "30d" }
-  );
-  const result = await User.create({ user, token });
-  return result;
+};
+const getSingleUserFromIntoDb = async (id: string) => {
+  try {
+    const result = await User.findById(id);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+const updateUserFormIntoDb = async (id: string, payload: IUser) => {
+  try {
+    const result = await User.findByIdAndUpdate(id, payload, { new: true });
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+const deleteUserFromIntoDb = async (id: string) => {
+  try {
+    const result = await User.findByIdAndDelete(id);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const userServices = {
+  getUsersFormIntoDb,
+  registerUserFromIntoDb,
+  loginFromUserIntoDb,
+  deleteUserFromIntoDb,
+  updateUserFormIntoDb,
+  getSingleUserFromIntoDb,
 };
